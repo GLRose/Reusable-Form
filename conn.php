9bfe41b2ab941e66284720d666944516ee1ed0f6
Garrett Rose <garrett.rose03@gmail.com>
2018-04-11 18:36:20 -0400

    Second ed

--- conn.php
+++ conn.php
@@ -11,19 +11,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-if(isset($_POST["submit"])){
+if(isset($_POST["fname"])){
 
 $stmt = $conn->prepare("INSERT INTO users (firstname,lastname,email,message) VALUES (?,?,?,?)");
 $stmt->bind_param("ssss",$firstname,$lastname,$email,$message);
 
 //setting parameters
-$firstname = $_POST['name'];
+$firstname = $_POST['fname'];
 $lastname = $_POST['lname'];
 $email = $_POST['mail'];
 $message = $_POST['note'];
 $stmt->execute();
 
-echo "New records created";
+
+
+echo "<p>New records created</p>";
+// header('Location: redirect.php');
 
 $stmt->close();
 $conn->close();
--- index.php
+++ index.php
@@ -1,20 +1,35 @@
-<?php
-include('conn.php');
-?>
 <!DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <title>Web Form</title>
         
     </head>
     <body>
-       <form action="" method="post">
-           First Name:<input type="text" name="name"></br>
-           Last Name:<input type="text" name="lname"></br>
-           Email:<input type="text" name="mail"></br>
-           Message:<input type="text" name="note"></br>
-           Click to submit <input type="submit" name="submit">
+    <div class="p-3 mb-2 bg-primary text-white">
+       <form action="" method="post" style="width: 500px; margin: auto">
+           <label for="fname" id="name">First Name</label>
+           <input type="text" name="fname" class="form-control">
+           <label for="lname" id="lname">Last Name</label>
+           <input type="text" name="lname" class="form-control">
+           <label for="mail" id="mail">Email</label>
+           <input type="text" name="mail" class="form-control">
+           <label for="note" id="note">Note</label>
+           <input type="text" name="note" class="form-control form-control-lg mb-2">
+           <div class="text-center"> 
+           <input type="submit" name="submit" class="btn btn-secondary btn-lg"></button>
+           </div>
        </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     </body>
 </html>
+<?php
+include('conn.php');
+?>
