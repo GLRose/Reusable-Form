7439883aa4bae75d633c539819287e2279b44f34
Garrett Rose <garrett.rose03@gmail.com>
2018-04-12 23:00:57 -0400

    Commented name validation stuffs

--- validate.php
+++ validate.php
@@ -3,12 +3,13 @@ $stmt = $conn->prepare("INSERT INTO users (firstname,lastname,email,message) VAL
 $stmt->bind_param("ssss",$firstname,$lastname,$email,$message);
 
 if(isset($_POST['submit'])){
-//setting parameters
+//setting POST variables to use
 $firstname = $_POST['fname'];
 $lastname = $_POST['lname'];
 $email = $_POST['mail'];
 $message = $_POST['note'];
 
+//checks for empty name fields in form
 if (empty($firstname) || empty($lastname)) {
     header("refresh:3;url=index.php");
     echo "<h1>Enter a first or last name</h1>";
